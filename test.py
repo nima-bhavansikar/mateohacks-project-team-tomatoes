8a8783eaf1c05f3b3ed592e7debcb904f30cf930
poibear <cool.aqualine@gmail.com>
2024-03-26 21:04:48 -0700

    cleanup repo

--- test.py
+++
@@ -1,6 +0,0 @@
-with open('supported_langs.txt') as newin:
-    languages = sorted(newin.read().splitlines())
-
-with open('langs.txt', "w") as newout:
-    for line in languages:
-        print(line, file=newout)
